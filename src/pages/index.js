5f3373e87dd821e1bc709dc73462796c510e436c
HariChidam <hchidam@umich.edu>
2024-03-03 19:18:42 -0500

    hari is awesome

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+
+
+export default function Home() {
+  return (
+      <div className=" pl-20 flex flex-row items-center text-center font-sans font-bold">
+          Hari is awesome
+      </div>
+  );
+}
